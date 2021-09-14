1d89ac95e6eed7b88c708b104f6aba3a2bb596ac
Alexander Solstad Larsen <Alexander.Solstad.Larsen@kartverket.no>
2021-09-14 11:12:09 +0200

    Fjernet AktiveringReminderTask

--- src/main/java/no/statkart/matrikkel/keycloak/scheduler/ScheduledTaskFactory.java
+++
@@ -1,6 +0,0 @@
-package no.statkart.matrikkel.keycloak.scheduler;
-
-import org.keycloak.provider.ProviderFactory;
-
-public interface ScheduledTaskFactory<T extends ScheduledTask> extends ProviderFactory<T> {
-}
