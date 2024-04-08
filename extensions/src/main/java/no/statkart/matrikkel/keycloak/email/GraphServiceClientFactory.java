6507af5755aa23e0faa2111b9213f1446d0c7bda
espgus <gustav.espegren@kartverket.no>
2024-04-08 13:48:39 +0200

    TH-1170: Bumper Microsoft-graf og azure-identity versjon i Keyclaok
    * Microsoft-graph: 5.75.0 -> 6.5.1
    * Azure-identity: 1.10.4 -> 1.11.4

--- extensions/src/main/java/no/statkart/matrikkel/keycloak/email/GraphServiceClientFactory.java
+++ extensions/src/main/java/no/statkart/matrikkel/keycloak/email/GraphServiceClientFactory.java
@@ -2,31 +2,23 @@ package no.statkart.matrikkel.keycloak.email;
 
 import com.azure.identity.ClientSecretCredential;
 import com.azure.identity.ClientSecretCredentialBuilder;
-import com.microsoft.graph.authentication.TokenCredentialAuthProvider;
-import com.microsoft.graph.requests.GraphServiceClient;
-import okhttp3.Request;
+import com.microsoft.graph.serviceclient.GraphServiceClient;
 
-import java.util.List;
 
 public class GraphServiceClientFactory {
 
     private GraphServiceClientFactory() {
     }
 
-    static GraphServiceClient<Request> create(OauthConfig oauthConfig) {
+    static GraphServiceClient create(OauthConfig oauthConfig) {
         final ClientSecretCredential clientSecretCredential = new ClientSecretCredentialBuilder()
                 .clientId(oauthConfig.getClientId())
                 .clientSecret(oauthConfig.getSecretId())
                 .tenantId(oauthConfig.getTenantId())
                 .build();
 
-        final TokenCredentialAuthProvider tokenCredentialAuthProvider = new TokenCredentialAuthProvider(
-                List.of("https://graph.microsoft.com/.default"), clientSecretCredential);
-        return GraphServiceClient
-                .builder()
-                .authenticationProvider(tokenCredentialAuthProvider)
-                .buildClient();
+
+        return new GraphServiceClient(clientSecretCredential, "https://graph.microsoft.com/.default");
 
     }
-
 }
