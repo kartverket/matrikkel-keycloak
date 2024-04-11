3e5105914a9d4ff1a966b48b75964eb3cbab8b89
espgus <gustav.espegren@kartverket.no>
2024-04-11 15:43:51 +0200

    TH-1315: testoppsett i keycloak med tester av CustomEndepunk
    * Tester for kartverk-opprettede endepunkter i extention

---
+++ extensions/src/integrationTest/java/no/statkart/matrikkel/keycloak/IntegrationTestConstants.java
@@ -0,0 +1,8 @@
+package no.statkart.matrikkel.keycloak;
+
+public class IntegrationTestConstants {
+    public static final String CLIENT_ID_SCHEDULER = "schedule-job";
+    public static final String CLIENT_ID_MATRIKKEL = "matrikkel";
+    public static final String CLIENT_SECRET_SCHEDULER = "secret";
+    public static final String CLIENT_SECRET_MATRIKKEL = "secret";
+}
